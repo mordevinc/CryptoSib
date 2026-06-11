01a36a69d5b6170c7ecfa346ef012f4d35803469
LizaM-525 <lizamorozova2511@gmail.com>
2026-06-12 01:07:06 +0700

    Add files via upload

--- permut/permut.h
+++ permut/permut.h
@@ -1,8 +1,14 @@
 #pragma once
-#ifndef PERMUT_H
-#define PERMUT_H
+#ifndef PERMUTATION_H
+#define PERMUTATION_H
 
-#include <string> 
+#ifdef PERMUTATION_EXPORTS
+#define PERMUTATION_API __declspec(dllexport)
+#else
+#define PERMUTATION_API __declspec(dllimport)
+#endif
+
+#include <string>
 #include <vector>
 
 struct PermutationCipher {
@@ -10,21 +16,21 @@ struct PermutationCipher {
     std::vector<int> order;
 };
 
-void initPermutationCipher(PermutationCipher* cipher, const std::string& key);
-void generateOrder(PermutationCipher* cipher);
-std::vector<int> stringKeyToOrderPermutation(const std::string& strKey);
-bool isNumericKeyPermutation(const std::string& key);
+PERMUTATION_API void initPermutationCipher(PermutationCipher* cipher, const std::string& key);
+PERMUTATION_API void generateOrder(PermutationCipher* cipher);
+PERMUTATION_API std::vector<int> stringKeyToOrderPermutation(const std::string& strKey);
+PERMUTATION_API bool isNumericKeyPermutation(const std::string& key);
 
-std::string encryptStringPermutation(const PermutationCipher* cipher, const std::string& text);
-std::string decryptStringPermutation(const PermutationCipher* cipher, const std::string& ciphertext);
+PERMUTATION_API std::string encryptStringPermutation(const PermutationCipher* cipher, const std::string& text);
+PERMUTATION_API std::string decryptStringPermutation(const PermutationCipher* cipher, const std::string& ciphertext);
 
-std::vector<char> encryptDataPermutation(const PermutationCipher* cipher, const std::vector<char>& data);
-std::vector<char> decryptDataPermutation(const PermutationCipher* cipher, const std::vector<char>& data);
+PERMUTATION_API std::vector<char> encryptDataPermutation(const PermutationCipher* cipher, const std::vector<char>& data);
+PERMUTATION_API std::vector<char> decryptDataPermutation(const PermutationCipher* cipher, const std::vector<char>& data);
 
-bool encryptFilePermutation(const PermutationCipher* cipher, const std::string& inputFile, const std::string& outputFile);
-bool decryptFilePermutation(const PermutationCipher* cipher, const std::string& inputFile, const std::string& outputFile);
+PERMUTATION_API bool encryptFilePermutation(const PermutationCipher* cipher, const std::string& inputFile, const std::string& outputFile);
+PERMUTATION_API bool decryptFilePermutation(const PermutationCipher* cipher, const std::string& inputFile, const std::string& outputFile);
 
-int getKeyLengthPermutation(const PermutationCipher* cipher);
-std::string getKeyPermutation(const PermutationCipher* cipher);
+PERMUTATION_API int getKeyLengthPermutation(const PermutationCipher* cipher);
+PERMUTATION_API std::string getKeyPermutation(const PermutationCipher* cipher);
 
-#endif // PERMUT_H
+#endif // PERMUTATION_H
