98ec7e731b19878ce7757e88b42fdd8a93b23cb8
LizaM-525 <lizamorozova2511@gmail.com>
2026-06-13 00:35:25 +0700

    Add files via upload

--- RC4/RC4.cpp
+++ RC4/RC4.cpp
@@ -1,124 +1,56 @@
 #include "rc4.h"
-#include <fstream>
-#include <filesystem>
-#include <sstream>
-#include <iomanip>
-#include <vector>
 
-using namespace std;
-
-void rc4InitState(uint8_t S[256], const string& key) {
+void rc4Init(const string& key, vector<unsigned char>& S) {
+    S.resize(256);
     for (int i = 0; i < 256; i++) {
         S[i] = i;
     }
     int j = 0;
     for (int i = 0; i < 256; i++) {
         j = (j + S[i] + key[i % key.size()]) % 256;
-        uint8_t temp = S[i];
-        S[i] = S[j];
-        S[j] = temp;
+        swap(S[i], S[j]);
     }
 }
 
-void rc4InitStateBytes(uint8_t S[256], const uint8_t* key, size_t keyLen) {
-    for (int i = 0; i < 256; i++) {
-        S[i] = i;
-    }
-    int j = 0;
-    for (int i = 0; i < 256; i++) {
-        j = (j + S[i] + key[i % keyLen]) % 256;
-        uint8_t temp = S[i];
-        S[i] = S[j];
-        S[j] = temp;
-    }
+unsigned char rc4Byte(vector<unsigned char>& S, int& i, int& j) {
+    i = (i + 1) % 256;
+    j = (j + S[i]) % 256;
+    swap(S[i], S[j]);
+    return S[(S[i] + S[j]) % 256];
 }
 
-void rc4ProcessData(uint8_t S[256], uint8_t* data, size_t length) {
+string rc4Crypt(const string& data, const string& key) {
+    vector<unsigned char> S;
+    rc4Init(key, S);
     int i = 0, j = 0;
-    for (size_t n = 0; n < length; n++) {
-        i = (i + 1) % 256;
-        j = (j + S[i]) % 256;
-        uint8_t temp = S[i];
-        S[i] = S[j];
-        S[j] = temp;
-        data[n] ^= S[(S[i] + S[j]) % 256];
+    string result;
+    for (char c : data) {
+        result.push_back(c ^ rc4Byte(S, i, j));
     }
+    return result;
 }
 
-void rc4ProcessVector(uint8_t S[256], vector<uint8_t>& data) {
-    rc4ProcessData(S, data.data(), data.size());
-}
-
-string rc4EncryptText(const string& text, const string& key) {
-    uint8_t S[256];
-    rc4InitState(S, key);
-    vector<uint8_t> data(text.begin(), text.end());
-    rc4ProcessVector(S, data);
-    return string(data.begin(), data.end());
-}
-
-string rc4DecryptText(const string& text, const string& key) {
-    return rc4EncryptText(text, key);
-}
-
-bool rc4EncryptFile(const string& inputFile, const string& outputFile, const string& key) {
+bool rc4EncryptFile(const string& input, const string& output, const string& key) {
     try {
-        std::filesystem::path outPath(outputFile);
+        fs::path outPath(output);
         if (!outPath.parent_path().empty()) {
-            if (!std::filesystem::exists(outPath.parent_path())) {
-                std::filesystem::create_directories(outPath.parent_path());
+            if (!fs::exists(outPath.parent_path())) {
+                fs::create_directories(outPath.parent_path());
             }
         }
-        ifstream in(inputFile, ios::binary);
+        ifstream in(input, ios::binary);
         if (!in.is_open()) return false;
-        vector<uint8_t> data((istreambuf_iterator<char>(in)), istreambuf_iterator<char>());
+        string data((istreambuf_iterator<char>(in)), istreambuf_iterator<char>());
         in.close();
-        uint8_t S[256];
-        rc4InitState(S, key);
-        rc4ProcessVector(S, data);
-        ofstream out(outputFile, ios::binary);
+        string encrypted = rc4Crypt(data, key);
+        ofstream out(output, ios::binary);
         if (!out.is_open()) return false;
-        out.write(reinterpret_cast<const char*>(data.data()), data.size());
+        out.write(encrypted.c_str(), encrypted.size());
         return true;
     }
     catch (...) { return false; }
 }
 
-bool rc4DecryptFile(const string& inputFile, const string& outputFile, const string& key) {
-    return rc4EncryptFile(inputFile, outputFile, key);
-}
-
-string rc4StringToHex(const string& str) {
-    stringstream ss;
-    ss << hex << setfill('0');
-    for (unsigned char c : str) {
-        ss << setw(2) << (int)c;
-    }
-    return ss.str();
-}
-
-string rc4HexToString(const string& hex) {
-    string result;
-    for (size_t i = 0; i < hex.size(); i += 2) {
-        if (i + 1 >= hex.size()) break;
-        char byte = static_cast<char>(stoi(hex.substr(i, 2), nullptr, 16));
-        result.push_back(byte);
-    }
-    return result;
-}
-
-string rc4EncrText(const string& text, const string& key) {
-    return rc4EncryptText(text, key);
-}
-
-string rc4DecrText(const string& text, const string& key) {
-    return rc4DecryptText(text, key);
-}
-
-bool rc4EncrFile(const string& input, const string& output, const string& key) {
+bool rc4DecryptFile(const string& input, const string& output, const string& key) {
     return rc4EncryptFile(input, output, key);
-}
-
-bool rc4DecrFile(const string& input, const string& output, const string& key) {
-    return rc4DecryptFile(input, output, key);
 }
--- RC4/RC4.h
+++ RC4/RC4.h
@@ -1,6 +1,4 @@
 #pragma once
-#ifndef RC4_H
-#define RC4_H
 
 #ifdef RC4_EXPORTS
 #define RC4_API __declspec(dllexport)
@@ -8,28 +6,16 @@
 #define RC4_API __declspec(dllimport)
 #endif
 
+#include <fstream>
+#include <iostream>
+#include <filesystem>
 #include <string>
 #include <vector>
-#include <cstdint>
+using namespace std;
+namespace fs = std::filesystem;
 
-RC4_API void rc4InitState(uint8_t S[256], const std::string& key);
-RC4_API void rc4InitStateBytes(uint8_t S[256], const uint8_t* key, size_t keyLen);
-RC4_API void rc4ProcessData(uint8_t S[256], uint8_t* data, size_t length);
-RC4_API void rc4ProcessVector(uint8_t S[256], std::vector<uint8_t>& data);
-
-RC4_API std::string rc4EncryptText(const std::string& text, const std::string& key);
-RC4_API std::string rc4DecryptText(const std::string& text, const std::string& key);
-
-RC4_API bool rc4EncryptFile(const std::string& inputFile, const std::string& outputFile, const std::string& key);
-RC4_API bool rc4DecryptFile(const std::string& inputFile, const std::string& outputFile, const std::string& key);
-
-RC4_API std::string rc4StringToHex(const std::string& str);
-RC4_API std::string rc4HexToString(const std::string& hex);
-
-RC4_API std::string rc4EncrText(const std::string& text, const std::string& key);
-RC4_API std::string rc4DecrText(const std::string& text, const std::string& key);
-
-RC4_API bool rc4EncrFile(const std::string& input, const std::string& output, const std::string& key);
-RC4_API bool rc4DecrFile(const std::string& input, const std::string& output, const std::string& key);
-
-#endif // RC4_H
+RC4_API void rc4Init(const string& key, vector<unsigned char>& S);
+RC4_API unsigned char rc4Byte(vector<unsigned char>& S, int& i, int& j);
+RC4_API string rc4Crypt(const string& data, const string& key);
+RC4_API bool rc4EncryptFile(const string& input, const string& output, const string& key);
+RC4_API bool rc4DecryptFile(const string& input, const string& output, const string& key);
